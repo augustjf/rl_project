24081afd231da039669b04245ef391e0aef38a9d
August Fors <augustjf@stud.ntnu.no>
2024-05-14 12:16:52 +0200

    Finished bulk of code. Need tuning and traning

--- final_project.py
+++ final_project.py
@@ -2,34 +2,45 @@ import gym
 import numpy as np
 import tensorflow as tf
 import tensorflow.python.keras as keras
+from tensorflow.python.keras.backend import argmax
 from tensorflow.python.keras.optimizers import adam_v2
 from collections import deque
 from tensorflow.python.keras import models, layers
-from tensorflow.python.keras.losses import MeanSquaredError
-from tensorflow.python.keras.optimizers import adam_v2
+from tensorflow.python.keras.losses import Huber
+from tensorflow.python.keras.models import save, load_model
+import matplotlib.pyplot as plt
 
 
 class DQN():
     def __init__(self):
         self.dropout = 0.2
         self.replay_memory_maxlen = 1000
-        self.replay_memory = deque([], maxlen=self.replay_memory_maxlen)
+        #deque is more efficient than list for this purpose
+        self.action_history = deque([], maxlen=self.replay_memory_maxlen)
+        self.state_history = deque([], maxlen=self.replay_memory_maxlen)
+        self.next_state_history = deque([], maxlen=self.replay_memory_maxlen)
+        self.rewards_history = deque([], maxlen=self.replay_memory_maxlen)
+        self.done_history = deque([], maxlen=self.replay_memory_maxlen)
+
 
     def create_model(self, state_space, action_space):
         model = models.Sequential()
-        model.add(layers.Conv2D(filters=32, kernel_size=8, strides=4, activation='relu', data_format='channels_last', input_shape=state_space))
-        model.add(layers.Conv2D(filters=64, kernel_size=3, strides=1, activation='relu'))
-        model.add(layers.MaxPooling2D(pool_size=(2,2)))
+        model.add(layers.Conv2D(filters=16, kernel_size=8, strides=4, activation='relu', data_format='channels_last', input_shape=state_space))
+        model.add(layers.Conv2D(filters=32, kernel_size=4, strides=2, activation='relu'))
+        #model.add(layers.MaxPooling2D(pool_size=(2,2)))
         model.add(layers.Flatten())
-        model.add(layers.Dropout(self.dropout))
-        model.add(layers.Dense(100))
+        #model.add(layers.Dropout(self.dropout))
+        model.add(layers.Dense(256))
         model.add(layers.Dense(action_space))   
         model.add(layers.Activation('softmax'))
-        model.compile(optimizer=adam_v2.Adam(), loss=MeanSquaredError) #optimizer might be wrong
         return model
 
-    def memory(self, state, action, reward, next_state):
-        self.replay_memory.append((state, action, reward, next_state))
+    def memory(self, state, action, reward, next_state, done):
+        self.action_history.append(action)
+        self.state_history.append(state)
+        self.next_state_history.append(next_state)
+        self.rewards_history.append(reward)
+        self.done_history.append(done)
 
     def output_to_action(self, output):
         if output == 0:
@@ -44,48 +55,130 @@ class DQN():
 
 class EnvironmentDQL():
     def __init__(self):
-        self.n_episodes = 10000
-        self.epsilon = 1
-        self.min_epsilon = 0.05
+        self.env = gym.make('procgen:procgen-fruitbot-v0', distribution_mode='easy', render_mode='human', use_backgrounds=False, num_levels=0)
+        self.n_train_episodes = 500
+        self.test_episodes = 100
+        self.batch_size = 32
+        self.update_target_network = 100
+        self.epsilon = 1.0
+        self.epsilon_min = 0.15
+        self.epsilon_max = 1.0
+        self.epsilon_random_frames = 5000
+        self.epsilon_exploration_frames = 100000
+        self.gamma = 0.99
         self.epsilon_decay = 0.999
+        self.learning_rate = 0.001
+        self.save_path = 'model.keras'
+        self.state_space = self.env.observation_space.shape
+        self.action_space = 3
+        self.episode_reward_history = []
+        self.optimizer = adam_v2.Adam(learning_rate=self.learning_rate)
+        self.loss_function = Huber()
+        
+        self.dqn = DQN()
+        self.train_dqn = self.dqn.create_model(self.state_space, self.action_space)
+        self.target_dqn = keras.models.clone_model(self.train_dqn)
 
 
     def train(self):
-        env = gym.make('procgen:procgen-fruitbot-v0', render_mode='human')
-        state_space = env.observation_space.shape
-        action_space = 3
-
-        dqn = DQN()
-        train_dqn = dqn.create_model(state_space, action_space)
-        target_dqn = keras.models.clone_model(train_dqn)
-
-        for i in range(self.n_episodes):
-            state = env.reset()
+        episode_count = 0
+        step_count = 0
+        for i in range(self.n_train_episodes):
+            state = self.env.reset()
             done = False
-
             action = 1
+            episode_reward = 0
+
             while(not done):
                 #Choose action based on epsilon greedy
                 r = np.random.random()
-                if r < self.epsilon:
-                    action = np.random.choice(action_space)
-                    print('Random action: ', action)
+                if step_count < self.epsilon_random_frames or r < self.epsilon:
+                    action = np.random.choice(self.action_space)
+                    #print('Random action: ', action)
                 else:
-                    action = train_dqn(state)
-                    print('DQN action: ', action)
+                    action = self.find_action(state)
                 
-                self.epsilon = max(self.min_epsilon, self.epsilon*self.epsilon_decay)
-
-                new_state, reward, done, _ = env.step(dqn.output_to_action(action))
-                dqn.memory.append((state, action, reward, new_state))
+                self.epsilon -= (self.epsilon_max - self.epsilon_min)/self.epsilon_exploration_frames
+                self.epsilon = max(self.epsilon_min, self.epsilon*self.epsilon_decay)
+            
+                new_state, reward, done, _ = self.env.step(self.dqn.output_to_action(action))
+                self.dqn.memory(state, action, reward, new_state, done)
                 state = new_state
+                episode_reward += reward
+                step_count += 1
 
+                if step_count > self.batch_size and step_count % 4 == 0:
+                    indecies = np.random.choice(range(len(self.dqn.done_history)), self.batch_size) #Use len(done_history) to get the number of samples in the replay memory
+                    state_sample = np.array(self.dqn.state_history)[indecies]
+                    next_state_sample = np.array(self.dqn.next_state_history)[indecies]
+                    rewards_sample = np.array(self.dqn.rewards_history)[indecies]
+                    action_sample = np.array(self.dqn.action_history)[indecies]
+                    done_sample = np.array(self.dqn.done_history)[indecies]
 
+                    pred_reward = self.train_dqn(next_state_sample)
+                    max_pred_rewards_ind = argmax(pred_reward, axis=1) #Can find better way to do this
+                    new_q_vals = rewards_sample + self.gamma*np.array(pred_reward[0])[max_pred_rewards_ind]
+                    for i, d in enumerate(done_sample): #If done, set the q value to the reward
+                        if d:
+                            new_q_vals[i] = rewards_sample[i]
 
+                    mask = tf.one_hot(action_sample, self.action_space)
+                    with tf.GradientTape() as tape:
+                        #Calculate gradients
+                        q_vals = self.train_dqn(state_sample)
+                        q_action = tf.reduce_sum(tf.multiply(q_vals, mask), axis=1)
+                        loss = self.loss_function(new_q_vals, q_action)
+                    
+                    grads = tape.gradient(loss, self.train_dqn.trainable_variables)
+                    self.optimizer.apply_gradients(zip(grads, self.train_dqn.trainable_variables))
 
+                if step_count % self.update_target_network == 0:
+                    self.target_dqn.set_weights(self.train_dqn.get_weights())
+                    print('Updated target network')
+                
+                if len(self.dqn.done_history) > self.dqn.replay_memory_maxlen:
+                    #Limit the length of the replay memory
+                    self.dqn.action_history.popleft(0)
+                    self.dqn.state_history.popleft(0)
+                    self.dqn.next_state_history.popleft(0)
+                    self.dqn.rewards_history.popleft(0)
+                    self.dqn.done_history.popleft(0)
+                
+                if done:
+                    break
 
+            episode_count += 1
+            self.episode_reward_history.append(episode_reward)
+            if len(self.episode_reward_history) > 100:
+                self.episode_reward_history.pop(0)
+            if episode_count % 5 == 0:
+                print('Episode: ', episode_count, 'Reward: ', episode_reward)
+                print('Epsilon: ', self.epsilon)
+                print('Mean reward: ', np.mean(self.episode_reward_history))
+                print('Frame count: ', step_count)
+
+        self.target_dqn.save(self.save_path)
+        self.env.close()
+
+    def test(self):
+        self.env = gym.make('procgen:procgen-fruitbot-v0', distribution_mode='easy', render_mode='human')
+        state = self.env.reset()
+        self.target_dqn = load_model(self.save_path)
+        for i in range(self.test_episodes):
+            action = self.find_action(state)
+            new_state, reward, done, _ = self.env.step(self.dqn.output_to_action(action))
+            state = new_state
+    
+    def find_action(self, state):
+        state_tensor = tf.convert_to_tensor(state)
+        state_tensor = tf.expand_dims(state_tensor, 0)
+        output_probs = self.target_dqn(state_tensor, training=False)
+        action = tf.argmax(output_probs[0]).numpy()
+        return action
 
 if __name__ == '__main__':  
     fruitbotDQL = EnvironmentDQL()
     fruitbotDQL.train()
+    plt.plot(fruitbotDQL.episode_reward_history)
+    fruitbotDQL.test()
 
