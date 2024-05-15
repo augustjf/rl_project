4746b6fb517e8a6c519106d9085d026c4e452a68
August Fors <augustjf@stud.ntnu.no>
2024-05-15 17:14:58 +0200

    Weights and data are now saved to json file

--- final_project.py
+++ final_project.py
@@ -1,4 +1,5 @@
 import gym
+import json
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
@@ -8,12 +9,13 @@ from tensorflow.python.keras.optimizers import adam_v2
 from tensorflow.python.keras import models, layers
 from tensorflow.python.keras.models import save_model, load_model
 from tensorflow.python.keras.losses import Huber
+from tensorflow.python.keras.models import model_from_json
 
 
 class DQN():
     def __init__(self):
         self.dropout = 0.2
-        self.replay_memory_maxlen = 1000
+        self.replay_memory_maxlen = 100000
         #deque is more efficient than list for this purpose
         self.action_history = deque([], maxlen=self.replay_memory_maxlen)
         self.state_history = deque([], maxlen=self.replay_memory_maxlen)
@@ -55,22 +57,25 @@ class DQN():
 class EnvironmentDQL():
     def __init__(self):
         self.env = gym.make('procgen:procgen-fruitbot-v0', distribution_mode='easy', use_backgrounds=False, num_levels=0)
-        self.n_train_episodes = 5
+        self.n_train_episodes = 1000
         self.test_episodes = 100
         self.batch_size = 32
         self.update_target_network = 100
         self.epsilon = 1.0
         self.epsilon_min = 0.1
         self.epsilon_max = 1.0
-        self.epsilon_random_steps = 5000
-        self.epsilon_exploration_steps = 100000
+        self.epsilon_random_steps = 10000
+        self.epsilon_exploration_steps = 1000000
         self.gamma = 0.99
         self.epsilon_decay = 0.999
         self.learning_rate = 0.001  
-        self.save_path = "model.h5"
+        self.save_weights_path = "weights.json"
+        self.save_data_path = "data.json"
+        self.data_dict = {}
         self.state_space = self.env.observation_space.shape
         self.action_space = 3
         self.episode_reward_history = []
+        self.q_action_history = []
         self.optimizer = adam_v2.Adam(learning_rate=self.learning_rate)
         self.loss_function = Huber()
         
@@ -127,6 +132,7 @@ class EnvironmentDQL():
                         #Calculate gradients
                         q_vals = self.train_dqn(state_sample)
                         q_action = tf.reduce_sum(tf.multiply(q_vals, mask), axis=1)
+                        self.q_action_history.append(q_action)
                         loss = self.loss_function(new_q_vals, q_action)
                     
                     grads = tape.gradient(loss, self.train_dqn.trainable_variables)
@@ -157,20 +163,23 @@ class EnvironmentDQL():
                 print('Mean reward: ', np.mean(self.episode_reward_history))
                 print('Step count: ', step_count)
 
-        save_model(self.train_dqn, self.save_path)
+        self.data_dict['episode_reward_history'] = self.episode_reward_history
+        self.data_dict['q_action_history'] = self.q_action_history
+        self.save_data_to_json()
+        save_weights_to_json(self.target_dqn, self.save_path)
+        
         self.env.close()
 
     def test(self):
         self.env = gym.make('procgen:procgen-fruitbot-v0', distribution_mode='easy', use_backgrounds=False, num_levels=0)
-        print('Env setup')
         state = self.env.reset()
-        #self.target_dqn = self.dqn.create_model(self.state_space, self.action_space)
-        self.target_dqn = load_model(self.save_path)
-        print('model loaded')
+        self.target_dqn = self.dqn.create_model(self.state_space, self.action_space)
+        self.target_dqn.set_weights(load_weights_from_json(self.save_path))
         for i in range(self.test_episodes):
             action = self.find_action(state)
             new_state, reward, done, _ = self.env.step(self.dqn.output_to_action(action))
             state = new_state
+        
     
     def find_action(self, state):
         state_tensor = tf.convert_to_tensor(state)
@@ -178,12 +187,47 @@ class EnvironmentDQL():
         output_probs = self.target_dqn(state_tensor, training=False)
         action = tf.argmax(output_probs[0]).numpy()
         return action
+    
+    def save_data_to_json(self):
+        with open(self.save_data_path, 'w', encoding='utf-8') as f:
+            json.dump(self.data_dict, f, ensure_ascii=False, indent=4)
+    
 
 def plot(data, title):
     plt.plot(data)
     plt.title(title)
     plt.show()
     plt.savefig(title + '.png')
+
+def save_weights_to_json(model, path):
+    weights_pre = model.get_weights()
+    weights_dict = {}
+    for i, w in enumerate(weights_pre):
+        weights_dict["layer_" + str(i)] = w.tolist()
+    with open(path, 'w', encoding='utf-8') as f:
+        json.dump(weights_dict, f, ensure_ascii=False, indent=4)
+
+    weights_post = load_weights_from_json(path)
+    flag = 0
+    for i in range(len(weights_post)):
+        if not np.equal(weights_pre[i], weights_post[i]).all():
+            flag = 1
+    if flag:
+        print('Weights not saved correctly')
+    else:
+        print('Weights saved correctly')
+
+
+def load_weights_from_json(path):
+    weights = []
+    with open(path) as f:
+        weights_dict = json.load(f)
+    for i in range(len(weights_dict)):
+        weights.append(np.array(weights_dict['layer_' + str(i)]))
+
+    return weights
+
+
 
 if __name__ == '__main__':  
     fruitbotDQL = EnvironmentDQL()
